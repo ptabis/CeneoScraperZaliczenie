1cbdf52ea1f47aa2c2574d3fd4e9d2d9f327d9d7
ptabis <p.tabis@protonmail.com>
2020-04-04 13:07:19 +0200

    select products rename res to opinions

--- templates/product/views.py
+++ templates/product/views.py
@@ -5,7 +5,11 @@ from Database import DB
 @app.route('/product/<id>')
 def product(id):
     cursor = DB.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM opinions WHERE id=%s", (id,))
-    res = cursor.fetchall()
-    print(res)
-    return render_template("product.html", subname=f"Produkt - {id}", id=id, mod="product", res=res)
+
+    cursor.execute("SELECT * FROM products WHERE id=%s", (id,))
+    products = cursor.fetchall()
+
+    cursor.execute("SELECT * FROM opinions WHERE product_id=%s", (id,))
+    opinions = cursor.fetchall()
+
+    return render_template("product.html", subname=f"Produkt - {id}", id=id, mod="product", opinions=opinions, products=products)
