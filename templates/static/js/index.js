5b0f3f1dcee9fa3361ab8471c40ccd5aac35d991
ptabis <p.tabis@protonmail.com>
2020-04-04 11:50:43 +0200

    add datatables move extract script to mod

--- templates/static/js/index.js
+++ templates/static/js/index.js
@@ -1,10 +1,21 @@
 import '../css/main.css';
+import $ from 'jquery';
+import 'datatables.net';
+import 'datatables.net-bs4/css/dataTables.bootstrap4.min.css';
 
 let mod = document.body.dataset['mod']
 switch(mod) {
     case 'home':
         break;
     case 'extract':
+        document.querySelector('#extraction_start').onclick = function(e) {
+            e.preventDefault()
+            window.location = '/extract/'+document.querySelector('#product_id').value
+        };
         break;
-
+    case "product":
+        $('#opinions-table').DataTable();
+        $('div.dataTables_filter input').addClass('form-control');
+        $('div.dataTables_filter select').addClass('form-control');
+        break;
 }
