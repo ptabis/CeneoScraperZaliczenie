7b535a8d794fcdc8b61434d1eaf3178e5666fdb0
ptabis <p.tabis@protonmail.com>
2020-04-04 13:34:16 +0200

    extract product

--- templates/extract/views.py
+++ templates/extract/views.py
@@ -1,16 +1,16 @@
 from templates import app
 from flask import render_template, redirect
-from Opinions import Opinions
+from Product import Product
 
 @app.route('/extract')
 def extract():
-    return render_template("extract.html", subname="Ekstrakcja opinii")
+    return render_template("extract.html", subname="Ekstrakcja opinii", mod="extract")
 
 @app.route('/extract/<id>')
 def extract_product(id):
     try:
-        opinions = Opinions.get_opinions(int(id))
-        opinions.insert_to_database()
+        product = Product.get_product(int(id))
+        product.insert_to_database()
     except:
         return render_template("extract.html", subname="Ekstrakcja opinii", mod="extract", error=1)
     return redirect('/product/'+str(id))
