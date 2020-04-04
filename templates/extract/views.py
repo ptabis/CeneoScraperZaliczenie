21c5f997645f9a07ebe31af20b804b81aa39fa37
ptabis <p.tabis@protonmail.com>
2020-04-04 11:48:50 +0200

    add extract view show error when extraction not succeeded use template

--- templates/extract/views.py
+++ templates/extract/views.py
@@ -1,6 +1,16 @@
 from templates import app
-from flask import render_template
+from flask import render_template, redirect
+from Opinions import Opinions
 
 @app.route('/extract')
 def extract():
     return render_template("extract.html", subname="Ekstrakcja opinii")
+
+@app.route('/extract/<id>')
+def extract_product(id):
+    try:
+        opinions = Opinions.get_opinions(int(id))
+        opinions.insert_to_database()
+    except:
+        return render_template("extract.html", subname="Ekstrakcja opinii", mod="extract", error=1)
+    return redirect('/product/'+str(id))
