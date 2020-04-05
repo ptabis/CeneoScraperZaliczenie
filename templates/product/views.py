7e9f3062b2763a031c2b481630872895d28632dd
ptabis <p.tabis@protonmail.com>
2020-04-05 14:01:48 +0200

    add opinion count and json download

--- templates/product/views.py
+++ templates/product/views.py
@@ -1,5 +1,5 @@
 from templates import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, jsonify
 from Database import DB
 
 @app.route('/products')
@@ -14,10 +14,29 @@ def products():
 def product(id):
     cursor = DB.cursor(dictionary=True)
 
-    cursor.execute("SELECT * FROM products WHERE id=%s", (id,))
+    cursor.execute("SELECT p.*, COUNT(o.id) AS opinions FROM products p LEFT JOIN opinions o ON p.id = o.product_id WHERE p.id=%s", (id,))
     product = cursor.fetchall()
 
     cursor.execute("SELECT * FROM opinions WHERE product_id=%s", (id,))
     opinions = cursor.fetchall()
 
     return render_template("product.html", subname=f"Produkt - {id} - {product[0]['name']}", id=id, mod="product", opinions=opinions, product=product[0])
+
+
+@app.route('/product/<id>/json')
+def product_json(id):
+    cursor = DB.cursor(dictionary=True)
+ 
+    cursor.execute("SELECT * FROM products p WHERE id=%s", (id,))
+    product = cursor.fetchall()
+
+    cursor.execute("SELECT * FROM opinions WHERE product_id=%s", (id,))
+    opinions = cursor.fetchall()
+
+    json = {
+        'product': product,
+        'opinions': opinions
+    }
+
+    return jsonify(json)
+
